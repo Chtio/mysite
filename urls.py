53d2977adc73bae0ee978056e2415850f15f60d2
Cedric FANGEUX <cedric@fangeux.com>
2011-05-16 15:00:14 +0200

    URL pattern

--- urls.py
+++ urls.py
@@ -13,5 +13,9 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^polls/$', 'polls.views.index'),
+    (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+    (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+    (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    (r'^admin/', include(admin.site.urls)),
 )
